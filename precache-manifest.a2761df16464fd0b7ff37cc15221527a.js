b24b53d76ded5c506dfbfb529b6bf5b21f8224b5
kalyan <116357692+amruthakalyan@users.noreply.github.com>
2024-10-17 16:48:40 +0530

    Updates

---
+++ precache-manifest.a2761df16464fd0b7ff37cc15221527a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f15b480f8a4f18e027f92c15f5c4bbc",
+    "url": "/bible-verses-api/index.html"
+  },
+  {
+    "revision": "7cbd725634d5b3387e32",
+    "url": "/bible-verses-api/static/js/2.cd2a726b.chunk.js"
+  },
+  {
+    "revision": "348f09741d4af4187658",
+    "url": "/bible-verses-api/static/js/main.0fb414ef.chunk.js"
+  },
+  {
+    "revision": "0cbecb5bd6ea0080baac",
+    "url": "/bible-verses-api/static/js/runtime~main.4c52f9af.js"
+  }
+]);
